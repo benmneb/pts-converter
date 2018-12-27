5eaa30db0b2a8ccd98f0ae0369745b99196030ad
olastor <>
2018-12-27 20:35:15 +0100

    update frontend and data script

--- data/prepare.js
+++ data/prepare.js
@@ -3,8 +3,8 @@ const PO = require('pofile')
 const cheerio = require('cheerio')
 const _ = require('lodash')
 const fs = require('fs')
-const dataLookup = {
-}
+const dataLookup = {}
+const dataStructure = {}
 
 const toRoman = {
   'i': 'i',
@@ -31,26 +31,53 @@ glob('../sc-data/po_text/pli-en/**/*.po', (err, files) => {
       if (ptsComment.length > 1) console.log('WHAT IS HAPPENINGT?')
       if (ptsComment.length > 0) {
         let pts = cheerio.load(ptsComment[0])('a.pts-vp-pli').attr('id')
-        const ptsBook = item.msgctxt.match(/^[a-z]+/gi)[0]
-        const chapter = item.msgctxt.split(':')[0]
 
-        if (pts !== undefined) {
-          pts = pts.replace('pts-vp-pl', '')
-          const ptsDiv = pts.split('.')[0]
-          const ptsNum = pts.split('.')[1]
-          if(toRoman[ptsDiv] === undefined) console.log(ptsComment)
-          _.set(dataLookup, `${ptsBook}.${toRoman[ptsDiv]}.${ptsNum}`, item.msgctxt.replace(ptsBook, ''))
-        } else {
-          // TODO
+        if (pts === undefined) {
+          return // TODO: what here?
+          // console.log(item.msgctxt)
+          // pts = ptsComment[0].match(/(?<=pts-vp-pl)i\d+\.\d+/gi)[0]
         }
+
+        const ptsBook = item.msgctxt.match(/^[a-z-]+/gi)[0].replace('pli-tv-', '')
+        let pts2 = pts.replace('pts-vp-pl', '')
+        const ptsDiv = pts2.split('.')[0]
+        const ptsNum = pts2.split('.')[1]
+        if(toRoman[ptsDiv] === undefined) console.log(ptsComment)
+
+        const newEntry = [item.msgctxt.split(':')[0], pts]
+        _.set(dataLookup, `${ptsBook}.${toRoman[ptsDiv]}.${ptsNum}`, newEntry)
       }
     })
   }))
 })
 
+const addToData = ($,t,suttaId) => {
+  let book = suttaId.match(/[a-z]+/gi)[0] // TODO: nomralize correct an => A
+  const cls = $(t).attr('class')
+  if (cls && cls !== 'pts') book += ` (${cls.replace('pts', '')})`
+  let number = parseInt($(t).attr('id').replace(cls, ''))
+
+  _.set(dataLookup, `${book}.${number}`, [suttaId, $(t).attr('id')])
+}
+
+glob('../sc-data/html_text/pli/**/*.html', (err, files) => {
+  files.map(file => {
+    let fileBase = file.split('/')
+    fileBase = fileBase[fileBase.length-1]
+
+    const $ = cheerio.load(fs.readFileSync(file))
+    const suttaId = $('section.sutta').attr('id')
+
+    if (!suttaId) console.log('No ID HERE', file)
+
+    $('article').find('.pts').map((_,t) => addToData($,t, suttaId))
+    $('article').find('.pts1ed').map((_,t) => addToData($,t, suttaId))
+    $('article').find('.pts2ed').map((_,t) => addToData($,t, suttaId))
+  })
+})
+
 setTimeout(() => {
-  fs.writeFileSync('pts_lookup.json', JSON.stringify(dataLookup))
-  const structure = { l1: Object.keys(dataLookup), l2: {}, l3: {} }
-  structure.l1.map(x => structure.l2[x] = Object.keys(dataLookup[x]).length)
-  Object.entries(structure.l2).map((k,v) => structure.l3[])
+  fs.writeFileSync('src/data/pts_lookup.json', JSON.stringify(dataLookup))
+  Object.keys(dataLookup).map(b => Object.keys(dataLookup[b]).map(c => _.set(dataStructure, `${b}.${c}`, dataLookup[b][c].length)))
+  fs.writeFileSync('src/data/pts_structure.json', JSON.stringify(dataStructure))
 }, 15 * 1000)
--- src/App.js
+++ src/App.js
@@ -1,25 +1,168 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ptsStructure from './data/pts_structure.json'
+import ptsData from './data/pts_lookup.json'
+import {
+  Card,
+  CardActions,
+  CardActionArea,
+  Typography,
+  CardContent,
+  FormControl,
+  Select,
+  MenuItem,
+  InputLabel,
+  Icon
+} from '@material-ui/core'
+import { get as gv } from 'lodash'
+
+const FROM_ROMAN = {
+  'i': 'i1',
+  'ii': 'i2',
+  'iii': 'i3',
+  'iv': 'i4',
+  'v': 'i5',
+  'vi': 'i6',
+  'vii': 'i7',
+  'viii': 'i8',
+  'ix': 'i9',
+  'x': 'i10'
+}
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      selectedBook: '',
+      selectedDiv: '',
+      selectedNum: ''
+    }
+  }
+
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  renderSelection() {
+    return <Card style={{ margin: '15px' }}>
+      <h3>PTS Converter</h3>
+      <p>Bitte die gesuchte PTS Stelle auswählen / Please select the pts position you want to look up:</p>
+      <FormControl>
+        <InputLabel>Book</InputLabel>
+        <Select
+          value={this.state.selectedBook}
+          style={{ minWidth: '80px' }}
+          onChange={(e) => this.setState({
+            selectedBook: e.target.value,
+            selectedDiv: '',
+            selectedNum: ''
+          })}
+        >
+          <MenuItem value=''>
+            <em>None</em>
+          </MenuItem>
+          { Object
+            .keys(ptsData)
+            .sort((a, b) => b.startsWith('b') ? -1 : a < b)
+            .map(k => <MenuItem value={k} key={k}>{k}</MenuItem>)}
+        </Select>
+      </FormControl>
+      <FormControl>
+        <InputLabel>Division</InputLabel>
+        <Select
+          value={this.state.selectedDiv}
+          style={{ minWidth: '80px' }}
+          onChange={(e) => this.setState({
+            selectedDiv: e.target.value,
+            selectedNum: ''
+          })}
+          disabled={!this.state.selectedBook}
+        >
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          { typeof ptsData[this.state.selectedBook] !== 'undefined'
+            ? Object
+              .keys(ptsData[this.state.selectedBook])
+              .map(k => <MenuItem value={k} key={k}>{k}</MenuItem>)
+            : null
+          }
+        </Select>
+      </FormControl>
+      <FormControl>
+        <InputLabel>Number</InputLabel>
+        <Select
+          name='selectedNum'
+          style={{ minWidth: '80px' }}
+          value={this.state.selectedNum}
+          onChange={(e) => this.setState({ selectedNum: e.target.value })}
+          disabled={!this.state.selectedBook && !this.state.selectedDiv}
+        >
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          { typeof ptsData[this.state.selectedBook] !== 'undefined'
+            && typeof ptsData[this.state.selectedBook][this.state.selectedDiv] !== 'undefined'
+            ? Object
+              .entries(ptsData[this.state.selectedBook][this.state.selectedDiv])
+              .filter(([k,v]) => v !== null)
+              .map(([k,v]) => <MenuItem value={k} key={k}>{k}</MenuItem>)
+            : null
+          }
+        </Select>
+      </FormControl>
+      <ul>
+        <li>Wegen der guten Aufbereitung der Daten von SuttaCentral sollte es möglich sein, dass beim Öffnen der ersten beiden Links (Pali + Sujato) auch an die richtige Stelle im Text gesprungen wird. Dies sollte bei dem Bodhi Link ebenfalls häufig funktionieren.</li>
+        <li>Es kann sein, dass eine PTS Stelle in den nächsten Text überläuft. Die Markierung ist nur der Anfang der Stelle. Durch Nachschlagen der nächst höheren PTS Nummer kann herausgefunden werden, wo die vorherige Stelle endet.</li>
+        <li>"Network Error" kann bedeuten, dass der Link nicht existiert (es gibt z.B. nicht immer Übersetzungen von Bodhi)</li>
+        <li>Fehler bitte melden</li>
+      </ul>
+    </Card>
+  }
+
+  renderResults() {
+    const result = gv(ptsData, `${this.state.selectedBook}.${this.state.selectedDiv}.${this.state.selectedNum}`, null)
+    if (result === null) {
+      return null
+    }
+
+    const sId = this.state.selectedBook + result.split(':')[0]
+    const sPos = `pts-vp-pl${FROM_ROMAN[this.state.selectedDiv]}.${this.state.selectedNum}`
+    const sPosBodhi = `${this.state.selectedBook}.${this.state.selectedDiv}.${this.state.selectedNum}`
+
+    return <Card style={{ margin: '15px' }}>
+      <CardActionArea>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            { sId.toUpperCase() }
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <a href={`https://suttacentral.net/${sId}/pli/ms#${sPos}`} target='_blank'>
+          Pali
+          <Icon>open_in_new</Icon>
+        </a>
+        <a href={`https://suttacentral.net/${sId}/en/sujato#${sPos}`} target='_blank'>
+          English (Sujato)
+          <Icon>open_in_new</Icon>
+        </a>
+        <a href={`https://suttacentral.net/${sId}/en/bodhi#${sPosBodhi}`} target='_blank'>
+          English (Bodhi)
+          <Icon>open_in_new</Icon>
+        </a>
+      </CardActions>
+    </Card>
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        { this.renderSelection() }
+        { this.renderResults() }
       </div>
     );
   }
