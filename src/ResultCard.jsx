8f9fd246738c5a59b501a24e56a9eb4fb9e091ae
Benjamin <benmneb@gmail.com>
2021-07-19 20:06:40 +0800

    attempt to change to hooks

--- src/ResultCard.jsx
+++ src/ResultCard.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 import {
   Card,
@@ -9,50 +9,48 @@ import {
   Icon,
 } from '@material-ui/core'
 
-export default class ResultCard extends Component {
-  render() {
-    const [suttaId, ptsRef] = this.props.data;
+export default function ResultCard({data}) {
+  const [suttaId, ptsRef] = data;
 
-    console.log('this.props.data', this.props.data)
-    console.log('suttaId', suttaId)
-    console.log('ptsRef', ptsRef)
-  
-    const bookId = suttaId.match(/[^0-9]+/gi);
+  console.log('props.data', data)
+  console.log('suttaId', suttaId)
+  console.log('ptsRef', ptsRef)
 
-    return (
-    <Card style={{ margin: '15px' }}>
-      <CardActionArea>
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            { suttaId.toUpperCase() }
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <a href={`https://suttacentral.net/${suttaId}/pli/ms#${ptsRef}`} target='_blank' rel="noopener noreferrer">
-          Pali
-          <Icon>open_in_new</Icon>
+  const bookId = suttaId.match(/[^0-9]+/gi);
+
+  return (
+  <Card style={{ margin: '15px' }}>
+    <CardActionArea>
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="h2">
+          { suttaId.toUpperCase() }
+        </Typography>
+      </CardContent>
+    </CardActionArea>
+    <CardActions>
+      <a href={`https://suttacentral.net/${suttaId}/pli/ms#${ptsRef}`} target='_blank' rel="noopener noreferrer">
+        Pali
+        {/* <Icon>open_in_new</Icon> */}
+      </a>
+      { bookId && ['mn', 'sn', 'an', 'dn', 'dhp', 'iti'].includes(bookId[0])
+        ? <a
+          href={`https://suttacentral.net/${suttaId}/en/sujato#${ptsRef}`}
+          target='_blank' rel="noopener noreferrer">
+          English (Sujato)
+          {/* <Icon>open_in_new</Icon> */}
         </a>
-        { bookId && ['mn', 'sn', 'an', 'dn', 'dhp', 'iti'].includes(bookId[0])
-          ? <a
-            href={`https://suttacentral.net/${suttaId}/en/sujato#${ptsRef}`}
-            target='_blank' rel="noopener noreferrer">
-            English (Sujato)
-            <Icon>open_in_new</Icon>
-          </a>
-          : null
-        }
-        { bookId && ['mn', 'sn', 'an'].includes(bookId[0])
-          ? <a
-            href={`https://suttacentral.net/${suttaId}/en/bodhi`}
-            target='_blank' rel="noopener noreferrer">
-            English (Bodhi)
-            <Icon>open_in_new</Icon>
-          </a>
-          : null
-        }
-      </CardActions>
-    </Card>
-    )
-  }
+        : null
+      }
+      { bookId && ['mn', 'sn', 'an'].includes(bookId[0])
+        ? <a
+          href={`https://suttacentral.net/${suttaId}/en/bodhi`}
+          target='_blank' rel="noopener noreferrer">
+          English (Bodhi)
+          {/* <Icon>open_in_new</Icon> */}
+        </a>
+        : null
+      }
+    </CardActions>
+  </Card>
+  )
 }
