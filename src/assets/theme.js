5e5291b8a8ede2b7449862bac0fdfcbdc1efa393
Benjamin <benmneb@gmail.com>
2021-07-20 22:45:58 +0800

    make font sizes responsive and fix tabsWrapper styling

--- src/assets/theme.js
+++ src/assets/theme.js
@@ -7,7 +7,6 @@ export const theme = createMuiTheme({
     primary: orange
   },
   typography: {
-    fontSize: 20,
     fontFamily: ['Inter', 'sans-serif'].join(','),
     button: {
       textTransform: 'none',
