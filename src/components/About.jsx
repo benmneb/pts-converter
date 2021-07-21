7dfd6e68441c24440685b50fdbc736bc4557645b
Benjamin <benmneb@gmail.com>
2021-07-21 19:13:46 +0800

    update CC0 img className

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -64,7 +64,7 @@ export default function About() {
           Attribution is appreciated but not legally required.
         </p>
         <a 
-          className="link"
+          className="footerLink"
           rel="license noopener noreferrer"
           href="http://creativecommons.org/publicdomain/zero/1.0/"
           target="_blank"
