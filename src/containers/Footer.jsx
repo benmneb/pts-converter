3d33e101dbf5e61b31b386fe1d97d56538b227b5
Benjamin <benmneb@gmail.com>
2021-07-21 17:19:11 +0800

    switch to function components

--- src/containers/Footer.jsx
+++ src/containers/Footer.jsx
@@ -1,22 +1,29 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 import Typography from '@material-ui/core/Typography'
 
-export default class Footer extends Component {
-  render() {
-    return (
-      <footer>
-        <Typography color="textSecondary">
-          <a 
-            className="footerLink"
-            href="https://github.com/benmneb"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            benmneb
-          </a>
-        </Typography>
-      </footer>
-    )
-  }
+export default function Footer() {
+  return (
+    <footer>
+      <Typography color="textSecondary">
+        <a 
+          className="footerLink"
+          rel="license noopener noreferrer"
+          href="http://creativecommons.org/publicdomain/zero/1.0/"
+          target="_blank"
+        >
+          CC0
+        </a> 
+        {' '}
+        <a 
+          className="footerLink"
+          href="https://github.com/benmneb"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          benmneb
+        </a>
+      </Typography>
+    </footer>
+  )
 }
--- src/containers/Header.jsx
+++ src/containers/Header.jsx
@@ -1,15 +1,13 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 import Typography from '@material-ui/core/Typography'
 
-export default class Header extends Component {
-  render() {
-    return (
-      <header>
-        <Typography variant="h4" component="h1">
-          Convert PTS Reference to Sutta Number
-        </Typography>
-      </header>
-    )
-  }
+export default function Header() {
+  return (
+    <header>
+      <Typography variant="h4" component="h1">
+        Convert PTS Reference to Sutta Number
+      </Typography>
+    </header>
+  )
 }
