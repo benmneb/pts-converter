ddc7f51147756ba34e9f1aefcc62b15234847732
Benjamin <benmneb@gmail.com>
2021-07-21 20:28:13 +0800

    Updates

---
+++ precache-manifest.2237de47b8dfbabd10574129aeed4e3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "56677d23e00831d8c1cb",
+    "url": "/pts-converter/static/css/main.b888dc3c.chunk.css"
+  },
+  {
+    "revision": "56677d23e00831d8c1cb",
+    "url": "/pts-converter/static/js/main.56677d23.chunk.js"
+  },
+  {
+    "revision": "4c19a7686d6ace2aade9",
+    "url": "/pts-converter/static/js/1.4c19a768.chunk.js"
+  },
+  {
+    "revision": "91c4dc233eae9376b199",
+    "url": "/pts-converter/static/js/runtime~main.91c4dc23.js"
+  },
+  {
+    "revision": "5a0f5aff7d1e25250af1e56bf69eb5cf",
+    "url": "/pts-converter/index.html"
+  }
+];
