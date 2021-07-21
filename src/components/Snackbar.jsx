78db722de755438aa746717197dc7248a382e5b9
Benjamin <benmneb@gmail.com>
2021-07-21 19:15:08 +0800

    add ability to copy sutta reference

---
+++ src/components/Snackbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+import MuiSnackbar from '@material-ui/core/Snackbar';
+
+import '../assets/styles.css'
+
+export default function Snackbar(props) {
+  const { open, close } = props;
+  
+  function handleClose(event, reason) {
+    if (reason !== 'clickaway') {
+      close();
+    }
+  };
+
+  return (
+    <MuiSnackbar
+      anchorOrigin={{
+        vertical: 'bottom',
+        horizontal: 'left',
+      }}
+      open={open}
+      autoHideDuration={3000}
+      onClose={handleClose}
+      ContentProps={{
+        'aria-describedby': 'message-id',
+      }}
+      message={<span id="message-id">Sutta reference copied to clipboard</span>}
+    />
+  )
+}
--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export {default as Results } from "./Results";
 export {default as Search } from "./Search";
 export {default as Select } from "./Select";
 export {default as About } from "./About";
+export {default as Snackbar } from "./Snackbar";
