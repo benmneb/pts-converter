cb4c34ae966f18e78d649ff2894ca0abc242164a
Benjamin <benmneb@gmail.com>
2021-07-23 07:41:36 +0800

    make redux dev tools work in production 🤞

--- src/state/store.js
+++ src/state/store.js
@@ -1,9 +1,9 @@
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
 
 import { reducer } from './reducer';
 
-export const store = createStore(
-  reducer,
-  process.env.NODE_ENV === 'development' &&
-    window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const enhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
+  : compose;
+
+export const store = createStore(reducer, enhancer);
