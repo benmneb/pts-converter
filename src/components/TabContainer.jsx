4b6715a340902de01f1878458c2127f0821eeade
Benjamin <benmneb@gmail.com>
2021-07-23 19:03:02 +0800

    switch div to section

--- src/components/TabContainer.jsx
+++ src/components/TabContainer.jsx
@@ -2,12 +2,12 @@ export default function TabContainer(props) {
   const { children, index, value } = props;
 
   return (
-    <div
+    <section
       id={`tabcontainer-${index}`}
       hidden={value !== index}
       className="tabContainer"
     >
       {value === index && children}
-    </div>
+    </section>
   );
 }
