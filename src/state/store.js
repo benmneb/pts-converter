e2f9f4367007d2e5256ecce377db556e4d9bd255
Benjamin <benmneb@gmail.com>
2021-07-24 19:16:01 +0800

    remove redux dev tools from production

--- src/state/store.js
+++ src/state/store.js
@@ -3,7 +3,7 @@ import { createStore, compose } from 'redux';
 import { reducer } from './reducer';
 
 const enhancer =
-  // process.env.NODE_ENV === 'development' &&
+  process.env.NODE_ENV === 'development' &&
   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
     : compose;
