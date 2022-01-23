a8a84057b9c7305383fd4707287d8bdf37326eca
benmneb <62878664+benmneb@users.noreply.github.com>
2022-01-23 13:54:51 +0800

    all references are now sentence case

--- src/utils/toSentenceCase.js
+++ src/utils/toSentenceCase.js
@@ -1,8 +1,4 @@
 export function toSentenceCase(str) {
-  if (['dt', 'pe', 'tha', 'thi', 'ya'].includes(str)) {
-    return str;
-  }
-
   return str
     .toLowerCase()
     .replace(/\s+([a-z])|^[a-z]/g, (s) => s.toUpperCase());
